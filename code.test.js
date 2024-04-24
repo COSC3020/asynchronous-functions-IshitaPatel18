7f6c3d76c2c8757647dedbcc55dd6952d9355639
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-04-24 15:38:51 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const jsc = require('jsverify');
-
+const assert = require('assert')
 eval(fs.readFileSync('code.js')+'');
+
 
 function regular(arr, element) {
   let total = 0;
@@ -18,6 +19,6 @@ function regular(arr, element) {
 const test =
     jsc.forall("array nat", "nat", function(arr, n) {
         return JSON.stringify(regular(arr, n)) ==
-            JSON.stringify(output(arr, n));
+            JSON.stringify(recieve(arr, n));
     });
 jsc.assert(test);
