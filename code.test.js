ecc630697fa8c62aa39fba48d93e122993fb6e96
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-04-24 16:18:04 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,38 +1,44 @@
 const fs = require('fs');
-const jsc = require('jsverify');
 const assert = require('assert')
+
 eval(fs.readFileSync('code.js')+'');
 
-const test1 = [1,2,1,1];
+// I looked at the code.test.js file from egkallas to understand how
+// these tests should be run if a callback is used
+
+let test1 = [1,2,1,1];
+let test2 = [3,3,3,3];
+let test3 = [2,5,4,3];
+let test4 = [0];
+let test5 = [0, 1, 2, 3, 4, 5, 6, 7];
+let test6 = [1,2,3,4];
+let test7 = [10,12,100,34,8,1;00]
+
 recieve(test1, 1, function(value) {
   assert(value === 3)
 });
 
-const test2 = [3,3,3,3];
 recieve(test2, 3, function(value) {
   assert(value === 4)
 });
 
-const test3 = [2,3,4,5];
 recieve(test3, 4, function(value) {
   assert(value === 1)
 });
 
-// function regular(arr, element) {
-//   let total = 0;
-//   for(let i = 0; i < arr.length; i++)
-//   {
-//     if(arr[i] == element)
-//     {
-//       total++
-//     }
-//   }
-//   return total;
-// }
+recieve(test4, 2, function(value) {
+  assert(value === 0)
+});
 
-// const test =
-//     jsc.forall("array nat", "nat", function(arr, n) {
-//         return JSON.stringify(regular(arr, n)) ==
-//             JSON.stringify(recieve(arr, n));
-//     });
-// jsc.assert(test);
+recieve(test5, 8, function(value) {
+  assert(value === 0)
+});
+
+recieve(test6, 1, function(value) {
+  assert(value === 1)
+});
+
+recieve(test7, 100, function(value) {
+  assert(value === 2)
+});
+
